e765b9bcd21e153846d323c6715adad190cfb6fa
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-07-19 23:58:24 +0530

    eye plays done

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Eyes = () => {
+function Eyes() {
+  const [rotate, setRotate] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
+
+      var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle);
+
+    });
+  });
+
   return (
     <div className="eyes w-full h-screen overflow-hidden">
       <div className="w-full relative h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg')]">
         <div className="absolute  flex gap-10  top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           <div className="w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-green-100">
-            <div className="w-[8vw] h-[8vw] relative  rounded-full bg-black">
-              <div className="w-full absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] h-8 ">
-              <div className="w-[2vw] h-[2vw] rounded-full bg-white"></div>
+            <div className="w-[8vw] h-[8vw] relative  rounded-full bg-black"> 
+              <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="w-full absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] h-8 ">
+                <div className="w-[2vw] h-[2vw] rounded-full bg-white"></div> 
               </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-green-100">
             <div className="w-[8vw] h-[8vw] relative rounded-full bg-black">
-              <div className="w-full absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  h-8">
-              <div className="w-[2vw] h-[2vw] rounded-full bg-white"></div>
+              <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}}  className="w-full absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  h-8">
+                <div className="w-[2vw] h-[2vw] rounded-full bg-white"></div>
               </div>
             </div>
           </div>
@@ -23,6 +39,6 @@ const Eyes = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Eyes;
